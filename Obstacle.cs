c0593d746a6f61160dd1ff157d703c3863f0ba71
Henrique Alves de Fernando <henrique160402@gmail.com>
2023-05-26 16:12:29 -0300

    construtor type

--- Obstacle.cs
+++ Obstacle.cs
@@ -2,4 +2,9 @@ public class Obstacle {
     //Alteracao realizada por ALTERADOR1
     private string type {get; set;}
     
+    //Outra alteracao realizada por ALTERADOR1
+    //Constructor:
+    Obstacle(string type){
+        this.type = type;
+    }
 }
