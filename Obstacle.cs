a5f905d733d498db1d8b2484f13b6654ce5f0bbc
Henrique Alves de Fernando <henrique160402@gmail.com>
2023-05-26 16:22:31 -0300

    construtor yPosition

--- Obstacle.cs
+++ Obstacle.cs
@@ -2,4 +2,9 @@ public class Obstacle {
     //Alteracao realizada por ALTERADOR3
     private int yPosition {get; set;}
 
+    //Outra alteracao realizada por ALTERADOR3
+    //Constructor:
+    Obstacle(int yPosition){
+        this.yPosition = yPosition;
+    }
 }
